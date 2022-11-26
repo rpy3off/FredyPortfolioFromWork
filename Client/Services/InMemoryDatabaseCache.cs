ab34b9cad3000134022a93d9ff3f58f30a2e2707
rPy3oFF <rPy3oFF@outlook.com>
2022-11-26 05:14:42 +0200

    Consuming the API

---
+++ Client/Services/InMemoryDatabaseCache.cs
@@ -0,0 +1,40 @@
+﻿using Shared.Models;
+using System.Net.Http.Json;
+
+namespace Client.Services
+{
+    internal sealed class InMemoryDatabaseCache
+    {
+        private readonly HttpClient _httpClient;
+
+        public InMemoryDatabaseCache(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        private List<Category> _categories = null;
+        internal List<Category> Categories 
+        { 
+            get 
+            {
+                return _categories; 
+            } 
+            set
+            {
+                _categories = value;
+                NotifyCategoriesDataCHanged();
+            }
+        }
+
+        internal async Task GetCategoriesFromDatabaseAndCache()
+        {
+            _categories = await _httpClient.GetFromJsonAsync<List<Category>>("endpoint");
+        }
+
+        internal event Action OnCategoriesDataChanged;
+        private void NotifyCategoriesDataCHanged()
+        {
+            OnCategoriesDataChanged?.Invoke();
+        }
+    }
+}
