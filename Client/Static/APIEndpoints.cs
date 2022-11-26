b29fcf259efae73f5850b1fa2f0140cb210ea589
rPy3oFF <rPy3oFF@outlook.com>
2022-11-26 23:25:49 +0200

    Update API Endpoint

--- Client/Static/APIEndpoints.cs
+++ Client/Static/APIEndpoints.cs
@@ -5,7 +5,7 @@
 #if DEBUG
         internal const string ServerBaseUrl = "https://localhost:7178";
 #else 
-        internal const string ServerBaseUrl = "https://appname.azurewebsite.net";
+        internal const string ServerBaseUrl = "https://fredyluskinserver.azurewebsites.net";
 #endif
 
         internal readonly static string s_categories = $"{ServerBaseUrl}/api/categories";
