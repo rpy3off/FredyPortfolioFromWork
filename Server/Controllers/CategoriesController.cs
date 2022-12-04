2d17063c72093a5ddbae9889af0b2c06e84dd4d2
rPy3oFF <rPy3oFF@outlook.com>
2022-12-04 19:58:29 +0200

    Added CRUD to Blog

--- Server/Controllers/CategoriesController.cs
+++ Server/Controllers/CategoriesController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Server.Data;
 using Shared.Models;
+using System.ComponentModel;
 
 namespace Server.Controllers
 {
@@ -10,11 +11,15 @@ namespace Server.Controllers
     public class CategoriesController : ControllerBase
     {
         private readonly AppDBContext _appDBContext;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public CategoriesController(AppDBContext appDBContext)
+        public CategoriesController(AppDBContext appDBContext, IWebHostEnvironment webHostEnvironment)
         {
             _appDBContext = appDBContext;
+            _webHostEnvironment = webHostEnvironment;
         }
+
+        #region CRUD operations
 
         [HttpGet]
         public async Task<IActionResult> Get()
@@ -22,6 +27,206 @@ namespace Server.Controllers
             List<Category> categories = await _appDBContext.Categories.ToListAsync();
 
             return Ok(categories);
+        }
+
+        // website.com/api/categories/withposts
+        [HttpGet("withposts")]
+        public async Task<IActionResult> GetWithPosts()
+        {
+            List<Category> categories = await _appDBContext.Categories
+                .Include(category => category.Posts)
+                .ToListAsync(); 
+
+            return Ok(categories);  
+        }
+
+        // website.com/api/categories/id
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            Category category = await GetCategoryByCategoryId(id, false);
+
+            return Ok(category);
+        }
+
+        // website.com/api/categories/id
+        [HttpGet("withposts/{id}")]
+        public async Task<IActionResult> GetWithPosts(int id)
+        {
+            Category category = await GetCategoryByCategoryId(id, true);
+
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Category categoryToCreate)
+        {
+            try
+            {
+                if (categoryToCreate == null)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                if (ModelState.IsValid == false)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                await _appDBContext.Categories.AddAsync(categoryToCreate);
+
+                bool changesPersistedToDatabase = await PersistChangesToDatabase();
+
+                if (changesPersistedToDatabase == false)
+                {
+                    return StatusCode(500, $"Something went wrong on our side. Please contact the admin.");
+
+                }
+                else
+                {
+                    return Created("Create", categoryToCreate);
+                }
+            }
+            catch (Exception e)
+            {
+
+                return StatusCode(500,$"Something went wrong on our side. Please contact the admin. Error message: {e.Message}.");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Create(int id, [FromBody] Category updatedCategory)
+        {
+            try
+            {
+                if (id < 1 || updatedCategory == null || id != updatedCategory.CategoryId)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                bool exists = await _appDBContext.Categories.AnyAsync(category => category.CategoryId == id);
+
+                if (exists == false)
+                {
+                    return NotFound();
+                }
+
+                if (ModelState.IsValid == false)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                _appDBContext.Categories.Update(updatedCategory);
+
+                bool changesPersistedToDatabase = await PersistChangesToDatabase();
+
+                if (changesPersistedToDatabase == false)
+                {
+                    return StatusCode(500, $"Something went wrong on our side. Please contact the admin.");
+
+                }
+                else
+                {
+                    return NoContent();
+                }
+            }
+            catch (Exception e)
+            {
+
+                return StatusCode(500, $"Something went wrong on our side. Please contact the admin. Error message: {e.Message}.");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                if (id < 1)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                bool exists = await _appDBContext.Categories.AnyAsync(category => category.CategoryId == id);
+
+                if (exists == false)
+                {
+                    return NotFound();
+                }
+
+                if (ModelState.IsValid == false)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                Category categoryToDelete = await GetCategoryByCategoryId(id, false);
+
+                if (categoryToDelete.ThumbnailImagePath != "uploads/placeholder.jpg")
+                {
+                    string fileName = categoryToDelete.ThumbnailImagePath.Split('/').Last();
+
+                    System.IO.File.Delete($"{_webHostEnvironment.ContentRootPath}\\wwwroot\\uploads\\{fileName}");
+                }
+
+                _appDBContext.Categories.Remove(categoryToDelete);
+
+                bool changesPersistedToDatabase = await PersistChangesToDatabase();
+
+                if (changesPersistedToDatabase == false)
+                {
+                    return StatusCode(500, $"Something went wrong on our side. Please contact the admin.");
+
+                }
+                else
+                {
+                    return NoContent();
+                }
+            }
+            catch (Exception e)
+            {
+
+                return StatusCode(500, $"Something went wrong on our side. Please contact the admin. Error message: {e.Message}.");
+            }
+        }
+
+        #endregion
+
+
+        #region Utility methods
+
+        [NonAction]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public async Task<bool> PersistChangesToDatabase()
+        {
+            int amountOfChanges = await _appDBContext.SaveChangesAsync();
+
+            return amountOfChanges > 0;
+        }
+
+
+        [NonAction]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        private async Task<Category> GetCategoryByCategoryId(int categoryId, bool withPosts)
+        {
+            Category categoryToGet = null;
+
+            if (withPosts == true)
+            {
+                categoryToGet = await _appDBContext.Categories
+                    .Include(category => category.Posts)
+                    .FirstAsync(category => category.CategoryId == categoryId);
+            }
+            else
+            {
+                categoryToGet = await _appDBContext.Categories
+                   .FirstAsync(category => category.CategoryId == categoryId);
+            }
+
+            return categoryToGet;
+
+            #endregion
+
+
         }
     }
 }
---
+++ Server/Data/Migrations/20221204170612_AddedThePostModel.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Data.Migrations
+{
+    public partial class AddedThePostModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    PostId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", maxLength: 128, nullable: false),
+                    ThumbnailImagePath = table.Column<string>(type: "TEXT", maxLength: 256, nullable: false),
+                    Excerpt = table.Column<string>(type: "TEXT", maxLength: 512, nullable: false),
+                    Content = table.Column<string>(type: "TEXT", maxLength: 65536, nullable: false),
+                    PublishDate = table.Column<string>(type: "TEXT", maxLength: 32, nullable: false),
+                    Published = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Author = table.Column<string>(type: "TEXT", maxLength: 128, nullable: false),
+                    CategoryId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.PostId);
+                    table.ForeignKey(
+                        name: "FK_Posts_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "PostId", "Author", "CategoryId", "Content", "Excerpt", "PublishDate", "Published", "ThumbnailImagePath", "Title" },
+                values: new object[] { 1, "Fredy", 1, "", "This is excerpt for post1. An excerpt ia a little extraction from a larger piece of text. Sort of like preview.", "04/12/2022 05:06", true, "uploads/placeholder.jpg", "First post" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "PostId", "Author", "CategoryId", "Content", "Excerpt", "PublishDate", "Published", "ThumbnailImagePath", "Title" },
+                values: new object[] { 2, "Fredy", 2, "", "This is excerpt for post2. An excerpt ia a little extraction from a larger piece of text. Sort of like preview.", "04/12/2022 05:06", true, "uploads/placeholder.jpg", "Second post" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "PostId", "Author", "CategoryId", "Content", "Excerpt", "PublishDate", "Published", "ThumbnailImagePath", "Title" },
+                values: new object[] { 3, "Fredy", 3, "", "This is excerpt for post3. An excerpt ia a little extraction from a larger piece of text. Sort of like preview.", "04/12/2022 05:06", true, "uploads/placeholder.jpg", "Third post" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "PostId", "Author", "CategoryId", "Content", "Excerpt", "PublishDate", "Published", "ThumbnailImagePath", "Title" },
+                values: new object[] { 4, "Fredy", 1, "", "This is excerpt for post4. An excerpt ia a little extraction from a larger piece of text. Sort of like preview.", "04/12/2022 05:06", true, "uploads/placeholder.jpg", "Fourth post" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "PostId", "Author", "CategoryId", "Content", "Excerpt", "PublishDate", "Published", "ThumbnailImagePath", "Title" },
+                values: new object[] { 5, "Fredy", 2, "", "This is excerpt for post5. An excerpt ia a little extraction from a larger piece of text. Sort of like preview.", "04/12/2022 05:06", true, "uploads/placeholder.jpg", "Five post" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "PostId", "Author", "CategoryId", "Content", "Excerpt", "PublishDate", "Published", "ThumbnailImagePath", "Title" },
+                values: new object[] { 6, "Fredy", 3, "", "This is excerpt for post6. An excerpt ia a little extraction from a larger piece of text. Sort of like preview.", "04/12/2022 05:06", true, "uploads/placeholder.jpg", "Six post" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_CategoryId",
+                table: "Posts",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
